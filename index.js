461f1d10a3fdd44bf7367c1bb99b542726c4b83c
Nicholas Adams <nicholasalexanderadams@gmail.com>
2018-06-06 23:58:14 -0400

    add a route to process a POST request to add an employee, so that data can begin to be added, so that there is something to work with.

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Full Stack Challenge'))
 
+// employee routes/actions
+
+app.post('/employee/add')
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
