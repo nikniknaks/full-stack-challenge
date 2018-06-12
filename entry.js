0d38489bf154e9b181560ba177e294d602c31481
Nicholas Adams <nicholasalexanderadams@gmail.com>
2018-06-12 14:25:11 +0100

    initial entry file for webpack with initial react UI.

---
+++ entry.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = (
+		<div>
+			<h1>Employees</h1>
+		</div>
+	)
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
