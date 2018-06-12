9e6cf021f84d9e374ec8013cd39c3e0f8f7ef1c2
Nicholas Adams <nicholasalexanderadams@gmail.com>
2018-06-12 14:34:05 +0100

    dom to display employees from database, with some controls to add, view or remove etc.

--- entry.js
+++ entry.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = (
-		<div>
-			<h1>Employees</h1>
-		</div>
-	)
-
 import axios from 'axios'
 
 axios.get('/employees').then(response => {
+	const employees = response.data.map(employee => 
+		<li>
+			<div>{employee.last_name}, {employee.first_name}; {employee.title}</div>
+			<a>View Employee</a>
+			<a>View Review</a>
+
+		</li>
+	);
+	const element = (
+			<div>
+				<h1>Employees</h1>
+				<ul>{employees}</ul>
+				<div class="addEmployee">
+					<h2>Add Employee</h2>
+					<label for="firstName">First Name: </label>
+					<input name="firstName" type="text"/>
+					<label for="lastName">Last Name: </label>
+					<input name="lastName" type="text"/>
+					<label for="title">Title: </label>
+					<input name="title" type="text"/>
+					<button>Add</button>
+				</div>
+			</div>
+
+		)
+
 	ReactDOM.render(
 	  element,
 	  document.getElementById('root')
