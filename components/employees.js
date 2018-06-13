195049c30e7b4282fc874631191f699e4c4bd0a2
Nicholas Adams <nicholasalexanderadams@gmail.com>
2018-06-14 00:05:54 +0100

    event listeners, event handlers and methods for setting local state and persisting in document database.

--- components/employees.js
+++ components/employees.js
@@ -7,6 +7,8 @@ export default class Employees extends React.Component {
 	constructor() {
 		super()
     this.addEmployee = this.addEmployee.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+
     this.state = {
     	employees: []
     }
@@ -20,9 +22,21 @@ export default class Employees extends React.Component {
 			console.log('this.state.employees', this.state.employees)
 		})
 	}
+
+	handleChange(event) {
+		this.setState({[event.target.name]: event.target.value});
+	}
+
 	addEmployee(event) {
-			.input.value
-		console.log(this)
+		axios.post('/employee/add', {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      title: this.state.title,
+		}).then(v => {
+			this.getEmployees();
+		}).catch(v => {
+			console.error(v);
+		});
 		event.preventDefault()
 	}
 	render() {
@@ -32,12 +46,12 @@ export default class Employees extends React.Component {
 				<EmployeeList employees={this.state.employees}/>
 				<form onSubmit={this.addEmployee} class="addEmployee">
 					<h2>Add Employee</h2>
-					<label for="firstName">First Name: </label>
-					<input name="firstName" type="text" ref={input => this.firstName = input} />
-					<label for="lastName">Last Name: </label>
-					<input name="lastName" type="text" ref={input => this.lastName = input} />
+					<label for="first_name">First Name: </label>
+					<input name="first_name" type="text" value={this.state.value} onChange={this.handleChange}/>
+					<label for="last_name">Last Name: </label>
+					<input name="last_name" type="text" value={this.state.value} onChange={this.handleChange}/>
 					<label for="title">Title: </label>
-					<input name="title" type="text" ref={input => this.title = input} />
+					<input name="title" type="text" value={this.state.value} onChange={this.handleChange}/>
 					<button type="submit" value="Submit">Add</button>
 				</form>
 			</div>
