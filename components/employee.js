c9087124f71cf242f567673dc41fc4453726f3ea
Nicholas Adams <nicholasalexanderadams@gmail.com>
2018-06-18 18:44:42 +0100

    get and display employee info in employee individual view.

--- components/employee.js
+++ components/employee.js
@@ -2,13 +2,23 @@ import React from 'react'
 
 export default class Employee extends React.Component {
 
-	constructor() {
-		super()
+	constructor(props) {
+		super(props)
+		this.state = {
+			'id': props.match.params.id
+		}
+		this.getEmployee()
 	}
 
 	render({ match }) {
 		return (
 			<div>
+				<h1>
+					Name: {this.state.last_name}, {this.state.first_name}
+				</h1>
+				<h2>
+					Title: {this.state.title}
+				</h2>
 		 </div>
 		)
 	}
