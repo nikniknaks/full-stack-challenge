b0a6a7a67d2323845bdbf2b83d89a69d49521e49
Nicholas Adams <nicholasalexanderadams@gmail.com>
2018-06-19 10:54:38 +0100

    rename file.

--- components/employee_request_list.js
+++ components/employee_request_list.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import RequestFeedback from '~/components/request_feedback'
 
 export default class EmployeeRequestList extends React.Component {
 	constructor(props) {
@@ -6,16 +7,19 @@ export default class EmployeeRequestList extends React.Component {
 	}
 
 	render() {
+
 		const otherEmployees = this.props.employees.filter(employee => {
 			return employee._id !== this.props.negateEmployeeId
 		})
+
 		const employeeItems = otherEmployees.map(employee => (
 				<li>
 					<div>{employee.last_name}, {employee.first_name}; {employee.title}</div>
-					<button href={'employee/' + employee._id}>Request Feedback</button>
+					<RequestFeedback />
 				</li>
 			)
 		)
+
 		return (
 			<ul>
 				{employeeItems}
