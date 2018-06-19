0f2b24f10420bb2b1d341fb6f8a0db0c7276455b
Nicholas Adams <nicholasalexanderadams@gmail.com>
2018-06-19 03:32:22 +0100

    review and delete employee.

--- components/employee.js
+++ components/employee.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Axios from 'axios'
 
+import Review from '~/components/review.js'
+
+import {
+  Link
+} from 'react-router-dom'
+
 export default class Employee extends React.Component {
 
 	constructor(props) {
@@ -9,50 +15,43 @@ export default class Employee extends React.Component {
 			'id': props.match.params.id
 		}
 		this.getEmployee()
+    this.deleteEmployee = this.deleteEmployee.bind(this);
 	}
 
 	getEmployee() {
-		Axios.get('/api/employee/' + this.state.id).then(r => {
-			this.setState(r.data)
-		})
-	}
-
-	writeReviewModeOn() {
-		this.setState({
-			'write_review_mode': true
-		})
-	}
-
-	writeReviewModeOff() {
-		this.setState({
-			'write_review_mode': false
+		Axios.get('/api/employee/' + this.state.id).then(response => {
+			this.setState(response.data)
 		})
 	}
 
 	submitReview() {
-		axios.post('/api/add_review', {
+		Axios.post('/api/add_review', {
 			'employeeId': this.state.employeeId,
 			'copy': this.state.copy,
 		})
 	}
 
+	deleteEmployee(event) {
+		event.preventDefault()
+		Axios.post('/api/employee/delete', {
+			employeeId: this.state.id,
+		}).then(response => {
+			this.getEmployee();
+		});
+	}
+
 	render() {
 		return (
 			<div>
+				<Link to='/'>Back to Employees</Link>
 				<h1>
 					Name: {this.state.last_name}, {this.state.first_name}
 				</h1>
 				<h2>
 					Title: {this.state.title}
 				</h2>
-			 <form>
-			 	<button onClick="writeReviewModeOn()">
-			 		Write Review
-			 	</button>
-			 	<button onClick="submitReview()">
-			 		Submit Review
-			 	</button>
-			 </form>
+				<Review id={this.state.id}/>
+				<button onClick={this.deleteEmployee}>Remove Employee</button>
 		 </div>
 		)
 	}
--- components/review.js
+++ components/review.js
@@ -1,11 +1,104 @@
 import React from 'react'
+import axios from 'axios'
+
+import {
+  Link
+} from 'react-router-dom'
 
 export default class Reviews extends React.Component {
-	constructor() {
+	constructor(props) {
+		super(props)
+		this.state = {
+			write_review_mode : false,
+			review : {
+				employeeId: this.props.id,
+				copy : '',
+			}
+		}
+		this.getReview()
+
+		this.writeReviewModeOn = this.writeReviewModeOn.bind(this)
+		this.writeReviewModeOff = this.writeReviewModeOff.bind(this)
+		this.assignFeedbackRequest = this.assignFeedbackRequest.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+		this.submitReview = this.submitReview.bind(this)
+	}
+
+	handleChange(event) {
+		this.setState({
+			review: {
+				[event.target.name]: event.target.value,
+				employeeId: this.props.id,
+			}
+		})
+	}
+
+	getReview() {
+		axios.get('/api/review/' + this.props.id).then(r => {
+			this.setState({
+				review : {
+					employeeId: this.props.id,
+					copy : r.data.copy
+				}
+			})
+		})
+	}
+
+	writeReviewModeOn(event) {
+		event.preventDefault()
+		this.setState({
+			'write_review_mode': true
+		})
+	}
+
+	writeReviewModeOff(event) {
+		event.preventDefault()
+		this.setState({
+			'write_review_mode': false
+		})
+	}
+
+	assignFeedbackRequest(event) {
+		event.preventDefault()
 
 	}
 
+	submitReview(event) {
+		event.preventDefault()
+		this.writeReviewModeOff(event)
+		axios.post('/api/review/add', this.state.review)
+	}
+
 	render() {
-		return 
+		let verb
+		if (this.state.review && this.state.review.copy && this.state.review.copy.length > 0) {
+	 		verb = <span>Edit</span>
+ 		} else {
+	 		verb = <span>Write</span>
+ 		}
+
+		if (this.state.write_review_mode) {
+			return (
+				<form>
+					<h3>Review</h3>
+					<textarea name="copy" type="text" value={this.state.review.copy} onChange={this.handleChange}></textarea>
+				 	<button onClick={this.submitReview}>Submit Review</button>
+				</form>
+			)
+		} else {
+			return (
+				<form>
+					<h3>Review</h3>
+				 	<button onClick={this.writeReviewModeOn}>
+				 		{verb}
+				 		<span> Review</span>
+				 	</button>
+				 	<Link to={'/feedback_request/' + this.props.id}>
+				 		Assign Feedback Request
+				 	</Link>
+				 	<p>{this.state.review.copy}</p>
+				</form>
+			)
+		}
 	}	
 }
